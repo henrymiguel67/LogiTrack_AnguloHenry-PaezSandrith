83be8cc06bc227c245ee1efc2d7dc9ebff567787
sandrypaez <sandrypaez0727@gmail.com>
2025-11-11 15:53:00 -0500

    feat: :sparkles: contenido de config

--- logitrack/src/main/java/com/c3/logitrack/config/AppConfig.java
+++ logitrack/src/main/java/com/c3/logitrack/config/AppConfig.java
@@ -1,5 +1,18 @@
 package com.c3.logitrack.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class AppConfig {
+
+    // Bean para codificar passwords con BCrypt, usado en seguridad
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     
 }
