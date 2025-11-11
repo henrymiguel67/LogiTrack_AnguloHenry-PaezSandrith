869c124963e50b064f977a1217ade0e873056a9b
sandrypaez <sandrypaez0727@gmail.com>
2025-11-11 16:33:40 -0500

    feat: :sparkles: codigo de model

--- logitrack/src/main/java/com/c3/logitrack/model/ReporteResumenDTO.java
+++ logitrack/src/main/java/com/c3/logitrack/model/ReporteResumenDTO.java
@@ -1,5 +1,17 @@
 package com.c3.logitrack.model;
 
 public class ReporteResumenDTO {
-    
+    private String bodega;
+    private int totalProductos;
+    private int totalMovimientos;
+
+    // Getters y setters
+    public String getBodega() { return bodega; }
+    public void setBodega(String bodega) { this.bodega = bodega; }
+
+    public int getTotalProductos() { return totalProductos; }
+    public void setTotalProductos(int totalProductos) { this.totalProductos = totalProductos; }
+
+    public int getTotalMovimientos() { return totalMovimientos; }
+    public void setTotalMovimientos(int totalMovimientos) { this.totalMovimientos = totalMovimientos; }
 }
