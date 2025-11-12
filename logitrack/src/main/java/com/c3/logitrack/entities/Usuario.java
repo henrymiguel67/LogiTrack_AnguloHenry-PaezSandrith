0d9f935ab22675c4a31c2c7932a5d71feee61354
Sandrith Carolina Páez Rincón <sandrypaez0727@gmail.com>
2025-11-12 18:10:12 -0500

    ✨ feat: optimiza clase Usuario eliminando getters redundantes

--- logitrack/src/main/java/com/c3/logitrack/entities/Usuario.java
+++ logitrack/src/main/java/com/c3/logitrack/entities/Usuario.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
     @Id
@@ -13,39 +19,9 @@ public class Usuario {
     private Long id;
 
     private String username;
+
     private String password;
+
     private String email;
 
-    // Getters y setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
