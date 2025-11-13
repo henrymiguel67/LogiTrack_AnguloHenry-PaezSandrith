e846480fd6468a21a48e987de99d80bc13e385e3
henrymiguel67 <henrymangulo12@gmail.com>
2025-11-13 15:20:16 -0500

    ✨ feat: extend MovimientoRepository with additional search methods for tipo and fecha

--- logitrack/src/main/java/com/c3/logitrack/repository/MovimientoRepository.java
+++ logitrack/src/main/java/com/c3/logitrack/repository/MovimientoRepository.java
@@ -1,13 +1,17 @@
 package com.c3.logitrack.repository;
 
 import com.c3.logitrack.entities.Movimiento;
-
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
-   Optional <Movimiento>  findByNombre(String nombre);
+
+    // Si deseas buscar por tipo (ya que la entidad lo tiene)
+    List<Movimiento> findByTipo(String tipo);
+
+    // Si quieres buscar por fecha exacta
+    List<Movimiento> findByFecha(java.util.Date fecha);
 }
